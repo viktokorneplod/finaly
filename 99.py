12888b44a40e12de2ccb898c727e30309fe70861
viktokorneplod <113383330+viktokorneplod@users.noreply.github.com>
2024-09-24 23:14:24 +0300

    Update 99.py

--- 99.py
+++ 99.py
@@ -1,7 +1,10 @@
+from random import *
+
 a = list(map(int, input('Введите сколько угодно чисел через пробел').split()))
 print('sum:', sum(a))
 print('dif:', a[0] - sum(a[1:len(a)]))
 
 
 print('sos')
+print(randint(1, 6))
 
